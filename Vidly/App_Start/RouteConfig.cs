6d151117278c1e7ef3cf829a898d59d04ae9874c
Hoàng Xuân Trí <hoangxuantri@outlook.com>
2018-08-10 15:41:11 +0700

    gitTest

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
 
             routes.MapRoute(
                 name: "Default",
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random(int id)
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+    }
+}
